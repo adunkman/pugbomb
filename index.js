821fa8cc3372f5364a25a8e98f37e438626e12f4
Andrew Dunkman <adunkman@Benny.local>
2012-01-18 00:00:51 -0600

    Compress css in production

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ var stylus = require('stylus'),
 
 app.configure(function () {
   app.set('view engine', 'jade');
+});
+
+app.configure('development', function () {
   app.use(stylus.middleware({ src: path.join(__dirname, 'public') }));
   app.use(express.static(path.join(__dirname, '/public')));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function () {
+  var oneYear = 31557600000;
+
+  app.use(stylus.middleware({ src: path.join(__dirname, 'public'), compress: true }));
+  app.use(express.static(path.join(__dirname, '/public'), { maxAge: oneYear }));
+  app.use(express.errorHandler());
 });
 
 app.get('/', pugstore.find(1), view.photo);
