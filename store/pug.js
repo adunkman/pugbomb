f217b900d0560b9e3dec9cfcce6aaa41c914d818
Pez Cuckow <email@pezcuckow.com>
2015-01-21 18:49:42 +0000

    Looks like these events are being bound more than once
    Was causing the server to fall over. Would like to track down the root
    cause and put in a better solution.
    
    "EventEmitter memory leak detected."

--- store/pug.js
+++ store/pug.js
@@ -4,10 +4,10 @@ var rest = require('restler'),
 var pugme = {
   random: function (callback) {
     rest.get('http://pugme.herokuapp.com/random')
-        .on('success', function (data) { callback(null, data['pug']); })
-        .on('fail', function (data) { callback(data); })
-        .on('error', function (error) { callback(error); })
-        .on('abort', function () { callback('Request aborted.'); });
+        .once('success', function (data) { callback(null, data['pug']); })
+        .once('fail', function (data) { callback(data); })
+        .once('error', function (error) { callback(error); })
+        .once('abort', function () { callback('Request aborted.'); });
   }
 };
 
